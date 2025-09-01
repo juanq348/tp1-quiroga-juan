8c5bb84def4a251352310ab7c2c1164a8c0bd564
Juan Q. <juanqi3489@gmail.com>
2025-09-01 20:36:02 -0300

    Se termino el punto 4 || Se hizo la clase Resultado || Se hizo la clase Partido

---
+++ src/models/Partido.ts
@@ -0,0 +1,40 @@
+import { Equipo } from "./Equipo";
+import { Deporte } from "./Deporte";
+import { Resultado } from "./Resultado";
+
+export class Partido { 
+    readonly id : number;
+    readonly local : Equipo;
+    readonly visitante : Equipo;
+    readonly deporte : Deporte;
+    private _resultado? : Resultado;
+
+    constructor(id : number, local : Equipo, visitante : Equipo , deporte : Deporte){
+    if(local === visitante){
+        throw new Error(`El equipo local no puede ser el mismo que el visitante`)
+    }
+        this.id = id;
+        this.local = local;
+        this.visitante = visitante;
+        this.deporte = deporte;
+    }
+
+    get resultado(): Resultado | undefined{
+        return this.resultado;
+    }
+
+    jugar(golesLocal:number, golesVisitante:number): void{
+        if(!this.deporte.validar(this.local)){
+            throw new Error(`El equipo local ${this.local.nombre} no cumple las reglas de ${this.deporte.nombre}`);
+        }
+        if(!this.deporte.validar(this.visitante)){
+            throw new Error(`El equipo visitante ${this.visitante.nombre} no cumple las reglas de ${this.deporte.nombre}`);
+        }
+        this._resultado = new Resultado(golesLocal, golesVisitante);
+    }
+
+    toString() : string{
+        const resultado = this._resultado ? `Resultado : ${this._resultado.toString()}` : "Todavia no se juega";
+        return `Partido [${this.id} || ${this.local.nombre} vs ${this.visitante.nombre} || ${this.deporte.nombre} -> ${resultado}]`
+    }
+}
